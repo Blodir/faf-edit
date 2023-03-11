21b6ade88fe7a861565a82d86445361c51539721
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2023-03-11 15:38:04 +0200

    remove redundant type casting

--- units.ts
+++ units.ts
@@ -12,35 +12,35 @@ const units: Unit[] = [
   {
     id: 'UEA0101',
     name: 'Hummingbird',
-    memberships: new Set<UnitSet>(['T1', 'air', 'scout', 'UEF'])
+    memberships: new Set(['T1', 'air', 'scout', 'UEF'])
   },
   {
     id: 'URA0101',
-    memberships: new Set<UnitSet>(['T1', 'air', 'scout', 'Cybran'])
+    memberships: new Set(['T1', 'air', 'scout', 'Cybran'])
   },
   {
     id: 'UAA0101',
-    memberships: new Set<UnitSet>(['T1', 'air', 'scout', 'Aeon'])
+    memberships: new Set(['T1', 'air', 'scout', 'Aeon'])
   },
   {
     id: 'XSA0101',
-    memberships: new Set<UnitSet>(['T1', 'air', 'scout', 'Seraphim'])
+    memberships: new Set(['T1', 'air', 'scout', 'Seraphim'])
   },
   {
     id: 'UEA0102',
-    memberships: new Set<UnitSet>(['T1', 'air', 'interceptor', 'UEF'])
+    memberships: new Set(['T1', 'air', 'interceptor', 'UEF'])
   },
   {
     id: 'URA0102',
-    memberships: new Set<UnitSet>(['T1', 'air', 'interceptor', 'Cybran'])
+    memberships: new Set(['T1', 'air', 'interceptor', 'Cybran'])
   },
   {
     id: 'UAA0102',
-    memberships: new Set<UnitSet>(['T1', 'air', 'interceptor', 'Aeon'])
+    memberships: new Set(['T1', 'air', 'interceptor', 'Aeon'])
   },
   {
     id: 'XSA0102',
-    memberships: new Set<UnitSet>(['T1', 'air', 'interceptor', 'Seraphim'])
+    memberships: new Set(['T1', 'air', 'interceptor', 'Seraphim'])
   },
 ];
 
